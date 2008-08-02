3daa1b5c41075b5e55d5621dc04efc569987e18c
Jesse Clark <jesse@jesseclark.com>
2008-08-02 14:00:33 -0700

    Created iphone project in xcode

---
+++ Classes/kuler_iphoneAppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  kuler_iphoneAppDelegate.m
+//  kuler-iphone
+//
+//  Created by Jesse Clark on 8/2/08.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import "kuler_iphoneAppDelegate.h"
+
+@implementation kuler_iphoneAppDelegate
+
+@synthesize window;
+
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {	
+	// Override point for customization after app launch
+}
+
+
+- (void)dealloc {
+	[window release];
+	[super dealloc];
+}
+
+@end

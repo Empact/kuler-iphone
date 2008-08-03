c3450370f4a136e6d60ca24ccea778437db829ba
Benjamin Ortuzar <benja@Silver.local>
2008-08-02 20:21:44 -0700

    adding RootViewController and TableViewCell, not tested yet

---
+++ native/Classes/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  kuler-iphone
+//
+//  Created by Benjamin Ortuzar on 02/08/2008.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UITableViewController {
+
+}
+
+@end
